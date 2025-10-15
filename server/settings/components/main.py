61dad609e75249b6cdc03d0b4d3cb96cd4420db1
AleksandrZhukovin <sashokzuk@gmail.com>
2025-10-15 15:52:58 +0300

    add(media): Set up media

--- server/settings/components/main.py
+++ server/settings/components/main.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'server.apps.core',
     'server.apps.blog',
 ]
 
@@ -127,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
