33f5858444c03ccf66af1f4a0d3ed81099b92e2e
AleksandrZhukovin <sashokzuk@gmail.com>
2025-10-15 15:44:57 +0300

    add(blog): Use time stamps for article models

--- server/apps/blog/models/article.py
+++ server/apps/blog/models/article.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+from server.apps.core.models import TimeStampedModel
 
-class Article(models.Model):
+
+class Article(TimeStampedModel):
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+
+class ArticleImage(TimeStampedModel):
+    article = models.ForeignKey('blog.Article', on_delete=models.CASCADE, related_name='images')
+
+    image = models.ImageField(upload_to='images/articles/')
